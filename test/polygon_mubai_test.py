1843a7e9b1d074a8aa2d54e7b6de63b7bb7b0c52
ZihangChen <chenzihang@gmail.com>
2022-08-29 10:33:39 -0400

    Adjust code and environment setting base on api responses

--- test/polygon_mubai_test.py
+++ test/polygon_mubai_test.py
@@ -5,7 +5,7 @@ import requests
 
 from test.Chain import Chain
 
-url_list = ['http://127.0.0.1:5000']
+url_list = ['https://calibration-mcs-api.filswan.com', 'https://calibration-mcs-bsc.filswan.com']
 
 @pytest.fixture
 def chain_info():
@@ -19,22 +19,26 @@ def server_config_info() -> Chain:
     server_config = []
 
     for url in url_list:
-        r = requests.get(url+'/config')
-        data = r.json()
-        server_config.append(Chain(data['environment'], network=data['network'],
+        r = requests.get(url+'/api/v1/common/system/params')
+        response = r.json()
+        data = response['data']
+        print(data)
+        server_config.append(Chain(url, network=data['chain_name'],
                             payment_contract_address=data['payment_contract_address'],
-                            signer_contract_address=data['signer_contract_address']))
+                            signer_contract_address=data['dao_contract_address']))
     return server_config
 
 
 # Need to test it polygon mumbai online matches the document defined in chain.toml
 def test_mumbai_config(chain_info, server_config_info):
+    keys = list(chain_info['environments'].keys())
+    n=0
     for server in server_config_info:
         print(chain_info)
-        env = server.environment
-        assert chain_info['environments'][env]['network'] == server.network
-        assert chain_info['environments'][env]['payment_contract_address'] == server.payment_contract_address
-        assert chain_info['environments'][env]['signer_contract_address'] == server.signer_contract_address
+        assert chain_info['environments'][keys[n]]['network'] == server.network
+        assert chain_info['environments'][keys[n]]['payment_contract_address'] == server.payment_contract_address
+        assert chain_info['environments'][keys[n]]['signer_contract_address'] == server.signer_contract_address
+        n+=1
 
 # Test if addresses and networks are unique (no reused addresses)
 def test_address_uniqueness(chain_info):
